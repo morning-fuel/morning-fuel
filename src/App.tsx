c86f15d8284f78ed7e32688d6df4b012a266f6c1
Satish Dabhi <satish.d@engineersmind.com>
2026-03-23 11:36:18 +0530

    Updated the browse-route to resolve the route related issue in gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/morning-fuel">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
